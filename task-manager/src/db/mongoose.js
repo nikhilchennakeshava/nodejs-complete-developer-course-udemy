71af2c75ffbbf557bfd2366a96df7f3d90783f66
nikhilcmurthy <nikhil9295@gmail.com>
2019-06-18 16:37:59 +0530

    http requests

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true,
@@ -7,36 +8,70 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 
 const User = mongoose.model('User', {
     name: {
-        type: String
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Email is invalid')
+            }
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 7,
+        trim: true,
+        validate(value) {
+            if (value.toLowerCase().includes('password')) {
+                throw new Error('Password is invalid. It cannot contain "password"')
+            }
+        }
     },
     age: {
-        type: Number
+        type: Number,
+        default: 0,
+        validate(value) {
+            if (value < 0) {
+                throw new Error('Age must be a positive number')
+            }
+        }
     }
 })
 
-// const user = new User({
-//     name: 'Nikhil',
-//     age: 24
-// })
+const user = new User({
+    name: 'Nikhil',
+    email: 'nikhil@TesT.com   ',
+    password: 'secretstuff',
+    age: 24
+})
 
-// user.save().then((result) => {
-//     console.log('Result:', result)
-// }).catch((error) => {
-//     console.log('Error:', error)
-// })
+user.save().then((result) => {
+    console.log('Result:', result)
+}).catch((error) => {
+    console.log('Error:', error)
+})
 
 const Task = mongoose.model('Task', {
     description: {
-        type: String
+        type: String,
+        required: true,
+        trim: true
     },
     completed: {
-        type: Boolean
+        type: Boolean,
+        default: false
     }
 })
 
 const task = new Task({
-    description: 'Mongoose library',
-    completed: false
+    description: 'Mongoose library       ',
 })
 
 task.save().then((result) => {
